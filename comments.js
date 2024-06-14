2675eda01e5dec83dd46d479a1078854fa98bd40
Prem <premnathj@gmail.com>
2024-06-14 19:11:53 +0530

    added comments

---
+++ comments.js
@@ -0,0 +1,16 @@
+// Create web server and listen on port 3000
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello, World!');
+});
+
+server.listen(3000, 'localhost', () => {
+    console.log('Server running at http://localhost:3000/');
+});
+
+
+
+
